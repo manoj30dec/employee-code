91013eb7c61c10619489b2e3f0fc7a2da6b75a52
manoj30dec <manojsoni.online@gmail.com>
2025-12-09 17:30:56 +0530

    added scss variable + ngbootstrap

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -9,12 +9,9 @@ import { Component, Input, input } from '@angular/core';
 export class Dashboard {
   // public usersList = input();
   @Input() public user: any;
+  @Input() public title: any;
 
   ngOnInit() {
-    //console.log("in dashboard component", this.usersList())
-    //this.usersList.update(obj => { obj.name = "Dashboard Modified Name" });
-    console.log("in dashboard component", this.user);
-    this.user.name = "Dashboard Modified Name";
-    console.log("after modification in dashboard component", this.user);
+
   }
 }
